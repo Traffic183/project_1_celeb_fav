26b748c0c1574153c4850a4a70dbc44a8609c104
Mara <levymaratx@gmail.com>
2018-07-24 16:21:30 -0500

    debug

--- appCeleb.js
+++ appCeleb.js
@@ -3,7 +3,7 @@ function apiCall(){
   var randomNumber = Math.floor((Math.random() * randomMovieArray.length - 1) + 1);
   var randomMovie = randomMovieArray[randomNumber];
 
-  $.getJSON('http://www.omdbapi.com/?t=' + encodeURI(randomMovie) + '&h=600&apikey=454a6e93').then(function(response){
+  $.getJSON('https://www.omdbapi.com/?t=' + encodeURI(randomMovie) + '&h=600&apikey=454a6e93').then(function(response){
   var image = response.Poster;
 
   if(image !== "N/A"){
