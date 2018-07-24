9a4f144f77aec8fb2bfd46021bc4881bc2e1a056
Mara <levymaratx@gmail.com>
2018-07-24 16:18:38 -0500

    debug

--- appCeleb.js
+++ appCeleb.js
@@ -2,10 +2,10 @@ var randomMovieArray = ['The Apartment', 'Vertigo', 'Breakfast at Tiffany’s', 
 function apiCall(){
   var randomNumber = Math.floor((Math.random() * randomMovieArray.length - 1) + 1);
   var randomMovie = randomMovieArray[randomNumber];
-console.log(randomMovie);
+
   $.getJSON('http://www.omdbapi.com/?t=' + encodeURI(randomMovie) + '&h=600&apikey=454a6e93').then(function(response){
   var image = response.Poster;
-console.log(image);
+
   if(image !== "N/A"){
     $('#adamsBack').attr('src', image);
     }
